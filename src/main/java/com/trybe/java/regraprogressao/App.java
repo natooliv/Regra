788c844bd949073f2c45b8077a99c94a05df06be
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2023-08-18 16:18:16 -0300

    Requisito 01

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,28 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int numeroAtividadesCadastradas = sc.nextInt();
+    sc.nextLine();
+
+    String[] nomeAtividades = new String[numeroAtividadesCadastradas];
+    int[] pesoAtividade = new int[numeroAtividadesCadastradas];
+    int somandoPesos = 0;
+    for (int i = 0; i < numeroAtividadesCadastradas; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      nomeAtividades[i] = sc.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      pesoAtividade[i] = sc.nextInt();
+      sc.nextLine();
+
+      somandoPesos += pesoAtividade[i];
+    }
+    if (somandoPesos != 100) {
+      System.out.println("Erro: A soma dos pesos não é igual a 100.");
+      return;
+    }
   }
 }
