7b41169a1f1263a7a5a3e888fb1048f71880dd2f
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2023-08-18 16:40:22 -0300

    Requisito 02

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -19,20 +19,43 @@ public class App {
 
     String[] nomeAtividades = new String[numeroAtividadesCadastradas];
     int[] pesoAtividade = new int[numeroAtividadesCadastradas];
+    int[] notasObtidas = new int[numeroAtividadesCadastradas];
     int somandoPesos = 0;
+
     for (int i = 0; i < numeroAtividadesCadastradas; i++) {
-      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      System.out.printf("Digite o nome da atividade %d:\n", i + 1);
       nomeAtividades[i] = sc.nextLine();
 
-      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      System.out.printf("Digite o peso da atividade %d:\n", i + 1);
       pesoAtividade[i] = sc.nextInt();
+      while (pesoAtividade[i] < 1 || pesoAtividade[i] > 100) {
+        System.out.println("Erro: Peso inválido! O peso deve ser entre 1 e 100.");
+        System.out.printf("Digite novamente o peso da atividade %d:\n", i + 1);
+        pesoAtividade[i] = sc.nextInt();
+      }
       sc.nextLine();
 
       somandoPesos += pesoAtividade[i];
+
+      System.out.printf("Digite a nota obtida para %s:\n", nomeAtividades[i]);
+      notasObtidas[i] = sc.nextInt();
+      while (notasObtidas[i] < 0 || notasObtidas[i] > 100) {
+        System.out.println("Erro: Nota inválida! A nota deve ser entre 0 e 100.");
+        System.out.printf("Digite novamente a nota obtida para %s:\n", nomeAtividades[i]);
+        notasObtidas[i] = sc.nextInt();
+      }
+      sc.nextLine();
     }
+
     if (somandoPesos != 100) {
       System.out.println("Erro: A soma dos pesos não é igual a 100.");
-      return;
     }
+
+    double mediaPonderada = 0;
+    for (int i = 0; i < numeroAtividadesCadastradas; i++) {
+      mediaPonderada += ((double) notasObtidas[i] * pesoAtividade[i]) / 100;
+    }
+
+    System.out.printf("A média ponderada do estudante é: %.2f%%\n", mediaPonderada);
   }
 }
